a838cdd3a5e07a2e457eab3b0d4d8cdb1a2e5982
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-05-10 16:03:10 -0300

    Update processMessage.ts

--- node/middlewares/processMessage.ts
+++ node/middlewares/processMessage.ts
@@ -1,23 +1,26 @@
 // The processMessage function gets the ticket message from the context and formats its content to be saved in a database later.
 
 import bodyParser from 'co-body'
-// import { saveMessage } from './saveMessage'
 
-export async function processMessage (ctx: Context, next: () => Promise<any>) {
+import { saveMessage } from './saveMessage'
 
+export async function processMessage(
+  ctx: Context,
+  next: () => Promise<Record<string, unknown>>
+) {
   console.info('Running processMessage')
 
-  //const request = ctx.request
-  const body = await bodyParser(ctx.req)
+  // const requestReceived = ctx.request
+  const messageReceived = await bodyParser(ctx.req)
 
-  console.info(body)
+  console.info(messageReceived)
 
-  // const response = await saveMessage(body)
-  // console.info('response')
-  // console.info(response)
+  const response = await saveMessage(messageReceived)
+
+  console.info('response')
+  console.info(response)
 
   ctx.status = 200
 
   await next()
-
 }
