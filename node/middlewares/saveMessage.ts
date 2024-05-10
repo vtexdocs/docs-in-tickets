7f10f60dc513639dd3fe8f919d330163ed4de9c7
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-05-10 15:23:00 -0300

    Update saveMessage.ts

--- node/middlewares/saveMessage.ts
+++ node/middlewares/saveMessage.ts
@@ -1,14 +1,20 @@
-// The saveMessage function gets the message data and saves it to our database.
-// import { HttpClient } from "@vtex/api"
-//
-// const url = 'https://analytics.vtex.com/api/analytics/schemaless-events'
-// const headers = {
-//   'Content-Type': 'application/json'
-// }
-//
-// export async function saveMessage (this: any, messageData: any) {
-//
-//   private http: HttpClient
-//
-//   return http.post()
-// }
+// The saveMessage function gets the message data and saves it to our RedShift database.
+
+const url = 'https://analytics.vtex.com/api/analytics/schemaless-events'
+const requestHeaders = {
+  'Content-Type': 'application/json',
+}
+
+export async function saveMessage(messageData: Record<string, unknown>) {
+  // eslint-disable-next-line no-console
+  console.log('Running saveMessage')
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: requestHeaders,
+    body: JSON.stringify(messageData),
+  })
+
+  // eslint-disable-next-line no-console
+  console.log(response.status)
+}
