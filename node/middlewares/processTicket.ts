bec3abe0cd92264651e763fe51e196e5ba92ecaf
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-05-23 18:09:45 -0300

    Update processTicket.ts

--- node/middlewares/processTicket.ts
+++ node/middlewares/processTicket.ts
@@ -19,9 +19,28 @@ export async function processTicket(
 
   const ticketComments = await zendesk.getComments(ticketId)
 
-  for (comment of ticketComments.comments) {
+  for (const comment of ticketComments.comments) {
+    let urls = []
+    let parsingArray = comment.html_body.split('href="')
+    let parsingString = parsingArray[1]
     const id = comment.id
-    const urls = comment.html_body.split('href"').split('"')[0]
+
+    if (typeof parsingString === 'undefined') {
+      console.info('undefined')
+      continue
+    }
+
+    console.info('Parsing comment: ' + id)
+    while (parsingArray.length > 1) {
+      console.info('loop')
+      console.info('length ' + parsingArray.length)
+      const parsedUrl = parsingArray[1].split('">')[0]
+
+      urls.push(parsedUrl)
+      parsingArray = parsingArray[2].split('href="')
+      console.info('urls array')
+      console.info(urls)
+    }
   }
 
   ctx.status = 200
