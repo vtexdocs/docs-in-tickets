218d1edf216b43f0e3ea6335baea6bd777eee9a4
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-05-24 20:26:30 -0300

    Update redshift.ts

--- node/clients/redshift.ts
+++ node/clients/redshift.ts
@@ -1,10 +1,24 @@
 // saveMessage gets the message data and saves it to our RedShift database.
 
-import { InfraClient, IOContext, InstanceOptions } from '@vtex/api'
+import { InfraClient } from '@vtex/api'
+import type { IOContext, InstanceOptions } from '@vtex/api'
 
 const url = 'https://analytics.vtex.com/api/analytics/schemaless-events'
 const requestHeaders = {
   'Content-Type': 'application/json',
+}
+
+export interface MessageData {
+  ticketId: string
+  commentId: number
+  authorId: number
+  createdAt: string
+  containsHelpArticle: boolean
+  containsDevArticle: boolean
+  numberOfDocsPortalsUrls: number
+  docsPortalsUrls: string[]
+  numberOfArticleUrls: number
+  articleUrls: string[]
 }
 
 export default class RedshiftClient extends InfraClient {
@@ -12,16 +26,22 @@ export default class RedshiftClient extends InfraClient {
     super('https://analytics.vtex.com', ctx, {
       ...options,
       retries: 2,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: requestHeaders,
     })
   }
 
-  public async saveMessage(messageData: Record<string, any>) {
-    return this.http.post(url, messageData, {
-      metric: 'save-ticket-message-doc-data',
-      headers: requestHeaders,
-    })
+  public async saveMessage(messageData: MessageData) {
+    return this.http.post(
+      url,
+      {
+        name: 'support-ticket-messages',
+        description: 'Information about a support ticket message containing documentation links.',
+        account: 'vtexhelp',
+        fields: messageData,
+      },
+      {
+        headers: requestHeaders,
+      }
+    )
   }
 }
