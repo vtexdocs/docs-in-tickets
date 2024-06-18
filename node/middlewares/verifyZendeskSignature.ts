c9f74a09fb8371b10c0ce2422ddecc535979efeb
PedroAntunesCosta <47991446+PedroAntunesCosta@users.noreply.github.com>
2024-06-18 14:06:14 -0300

    Update verifyZendeskSignature.ts

--- node/middlewares/verifyZendeskSignature.ts
+++ node/middlewares/verifyZendeskSignature.ts
@@ -15,14 +15,19 @@ export async function verifyZendeskSignature (
   // Determining whether the signature is compatible with the expected, according to the secret key provided by zendesk. This key is exclusive to this webhook.
   function isValidSignature(signature: string, body: string, timestamp: string): boolean {
 
-    const hmac = crypto.createHmac(SIGNING_SECRET_ALGORITHM, ZENDESK_SECRET_KEY)
-    const sig = hmac.update(timestamp + body).digest('base64')
+    console.info('Running isValidSignature')
+    let hmac = crypto.createHmac(SIGNING_SECRET_ALGORITHM, ZENDESK_SECRET_KEY)
+    let sig = hmac.update(timestamp + body).digest('base64')
+    const comparison = Buffer.compare(
+      Buffer.from(signature, 'base64'),
+      Buffer.from(sig.toString(), 'base64')
+    )
+    console.info(signature)
+    console.info(sig)
+    console.info(comparison)
 
     return (
-      Buffer.compare(
-        Buffer.from(signature, 'base64'),
-        Buffer.from(sig, 'base64')
-      ) === 0
+       comparison === 0
     )
   }
 
@@ -30,10 +35,6 @@ export async function verifyZendeskSignature (
     const requestBody = await bodyParser(ctx.req)
     const requestSignature: string = ctx.request.headers['x-zendesk-webhook-signature'] as string
     const requestSignatureTimestamp: string = ctx.request.headers['x-zendesk-webhook-signature-timestamp'] as string
-    console.info('zendesk signature')
-    console.info(requestSignature)
-    console.info('zendesk signature timestamp')
-    console.info(requestSignatureTimestamp)
 
     if (isValidSignature(requestSignature, requestBody, requestSignatureTimestamp)) {
 
